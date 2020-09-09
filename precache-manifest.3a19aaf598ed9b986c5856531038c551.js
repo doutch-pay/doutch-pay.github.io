677e66bc97dea68b3b1c377d138f01239faefc97
Aria <christina.ohari@gmail.com>
2020-09-09 15:27:57 +0900

    Updates

---
+++ precache-manifest.3a19aaf598ed9b986c5856531038c551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7683f5bfa025fdbb3e257e238d0e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4d920bfacf570e10e57",
+    "url": "/static/css/main.f81b6dfd.chunk.css"
+  },
+  {
+    "revision": "d9dfce9bdff92ad14959",
+    "url": "/static/js/2.02d9fded.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.02d9fded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d920bfacf570e10e57",
+    "url": "/static/js/main.2c217b12.chunk.js"
+  },
+  {
+    "revision": "23486a3a9341297a2d60",
+    "url": "/static/js/runtime-main.75d17864.js"
+  }
+]);
